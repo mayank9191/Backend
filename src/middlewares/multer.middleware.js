593b7cdbe5c647a71f223832b4a8445f7573879b
Mayank Kulahara <mayankkulahara@gmail.com>
2025-03-31 21:22:00 +0530

    add comment

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,5 +1,6 @@
 import multer from 'multer';
 
+// it is used to store a file in server locally as middleware from form data 
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
