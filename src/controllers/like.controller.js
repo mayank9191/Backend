4dc17b2e47d581b77aa1f69bba7adb9270e9f62c
Mayank Kulahara <mayankkulahara@gmail.com>
2025-04-05 23:51:17 +0530

    feat(like.controller.js): add validation and response handling for toggling likes on videos, comments, and tweets

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -7,17 +7,74 @@ import { Like } from '../models/like.model.js'
 
 // toggle like on video
 const toggleVideoLike = asyncHandler(async (req, res) => {
+  const { videoId } = req.params
+  const userId = req.user?._id
+
+  if (!userId || !mongoose.Types.ObjectId.isValid(userId)) {
+    throw new ApiError(400, "user id not valid")
+  }
+
+  if (!videoId || !mongoose.Types.ObjectId.isValid(videoId)) {
+    throw new ApiError(400, "video id not valid")
+  }
+
+  const likeStatus = await Like.create({
+    video: videoId,
+    likedBy: userId
+  })
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, newLike, "Video liked"))
 
 })
 
 
 // toggle like on comment
 const toggleCommentLike = asyncHandler(async (req, res) => {
+  const { videoId } = req.params
+  const userId = req.user?._id
+
+  if (!userId || !mongoose.Types.ObjectId.isValid(userId)) {
+    throw new ApiError(400, "user id not valid")
+  }
+
+  if (!videoId || !mongoose.Types.ObjectId.isValid(videoId)) {
+    throw new ApiError(400, "video id not valid")
+  }
+
+  const likeStatus = await Like.create({
+    video: videoId,
+    likedBy: userId
+  })
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, newLike, "Video liked"))
 
 })
 
 // toggle like on tweet
 const toggleTweetLike = asyncHandler(async (req, res) => {
+  const { videoId } = req.params
+  const userId = req.user?._id
+
+  if (!userId || !mongoose.Types.ObjectId.isValid(userId)) {
+    throw new ApiError(400, "user id not valid")
+  }
+
+  if (!videoId || !mongoose.Types.ObjectId.isValid(videoId)) {
+    throw new ApiError(400, "video id not valid")
+  }
+
+  const likeStatus = await Like.create({
+    video: videoId,
+    likedBy: userId
+  })
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, newLike, "Video liked"))
 
 })
 
