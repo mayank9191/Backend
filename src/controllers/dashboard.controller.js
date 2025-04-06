8e4c4455b90a85c0062446407cb5954b4af7fe43
Mayank Kulahara <mayankkulahara@gmail.com>
2025-04-07 02:34:36 +0530

    feat(dashboard.controller.js): add channel stats and videos retrieval with validation for channel ID

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -11,6 +11,88 @@ import { Like } from "../models/like.model.js"
 // Get the channel stats like total video views, total subscribers, total videos, total likes etc.
 
 const getChannelStats = asyncHandler(async (req, res) => {
+  const { channelId } = req.query
+
+  if (!channelId || !mongoose.Types.ObjectId.isValid(channelId)) {
+    throw new ApiError(400, "channel id is not valid")
+  }
+
+  // channel total video views through aggregation pipeline
+
+  const channelTotalVideoViewsAgg = await Video.aggregate([
+    {
+      $match: { owner: new mongoose.Types.ObjectId(channelId) } // match the feild required
+    },
+    {
+      $group: { // group all videos founded views and save in totalViews by summing $views feild
+        _id: null,
+        totatViews: { $sum: "$views" }
+      }
+    }
+
+  ])
+
+  // channel total subscribers
+
+  const channelTotalSubscriberAgg = await Subscription.aggregate([
+    {
+      $match: {
+        channel: new mongoose.Types.ObjectId(channelId)
+      }
+    },
+    {
+      $group: {
+        _id: null,
+        totalSubscriber: {
+          $sum: 1 // "$sum: 1" is used inside a "$group" stage to count documents. It adds 1 for each document that passes through the pipeline 
+        }
+      }
+    }
+  ])
+
+  // channel total video videos
+
+  const channelTotalVideosAgg = await Video.aggregate([
+    {
+      $match: { owner: new mongoose.Types.ObjectId(channelId) }
+    },
+    {
+      $group: {
+        _id: null,
+        totalVideos: {
+          $sum: 1
+        }
+      }
+    }
+  ])
+
+  // channel total videos likes
+
+  const channelTotalVideoLikesAgg = await Video.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(channelId)
+      }
+    },
+    {
+      $group: {
+        _id: null,
+        totalVideoLike:
+      }
+    }
+
+  ])
+
+  const channelStats = {
+    totalVideoViews: channelTotalVideoViewsAgg[0]?.totatViews || 0,
+    totalSubscriberCount: channelTotalSubscriberAgg[0]?.totalSubscriber || 0,
+    totalVideos: channelTotalVideosAgg[0]?.totalVideos || 0
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, channelStats, "total video views fetched successfully"))
+
 
 })
 
@@ -19,6 +101,21 @@ const getChannelStats = asyncHandler(async (req, res) => {
 // Get all the videos uploaded by the channel
 
 const getChannelVideos = asyncHandler(async (req, res) => {
+  const { channelId } = req.query
+
+  if (!channelId || !mongoose.Types.ObjectId.isValid(channelId)) {
+    throw new ApiError(400, "channel id is not valid")
+  }
+
+  const channelVideos = await Video.find({
+    owner: channelId,
+    isPublished: true
+  }).sort({ createdAt: -1 })
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, channelVideos, "channel all videos fetched successfully"))
+
 
 })
 
